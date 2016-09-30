9177ee6352e76fc62379e0ffd10077a65722d64f
Oliver Prince <oprince14@gmail.com>
2016-09-30 03:52:37 -0500

    User home page tabs (angularjs) added

--- src/javascripts/controllers/HomeCtrl.js
+++ src/javascripts/controllers/HomeCtrl.js
@@ -5,5 +5,24 @@
     .controller('HomeCtrl', function($scope, $location) {
       $scope.active = $location.path();
       $scope.showNav = true;
+      $scope.tab = 'day';
+      $scope.tabs = [{
+        bat: 'day',
+        title: 'Day'
+      }, {
+        bat: 'week',
+        title: 'Week'
+      }, {
+        bat: 'month',
+        title: 'Month'
+      }];
+      $scope.tabSelect = function(bat) {
+        console.log(bat);
+        $scope.tab = bat;
+      };
+      $scope.checkTab = function (tab) {
+        console.log(tab);
+        return $scope.tab === tab;
+      };
     });
 }());
